603c6d5847bb5d9b4ca3aee3ca46a27fa8f46525
erafaelmanuel <erafaelmanuel@gmail.com>
2017-12-01 16:09:03 +0800

    Continue to refactor the class resource.

--- src/main/java/io/ermdev/alice/controller/ClassController.java
+++ src/main/java/io/ermdev/alice/controller/ClassController.java
@@ -1,4 +1,53 @@
 package io.ermdev.alice.controller;
 
+import io.ermdev.alice.model.Class;
+import io.ermdev.alice.repository.ClassRepository;
+import io.ermdev.alice.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class ClassController {
+
+    private ClassRepository classRepository;
+    private StudentRepository studentRepository;
+
+    @Autowired
+    public ClassController(ClassRepository classRepository, StudentRepository studentRepository) {
+        this.classRepository = classRepository;
+        this.studentRepository = studentRepository;
+    }
+
+    @GetMapping("class/{classId}")
+    public Class getById(@PathVariable("classId") Long classId) {
+        return classRepository.findOne(classId);
+    }
+
+    @GetMapping("class/all")
+    public List<Class> getAll() {
+        return classRepository.findAll();
+    }
+
+    @PostMapping("class")
+    public Class add(Class _class) {
+        return classRepository.save(_class);
+    }
+
+    @PutMapping("class/{classId}")
+    public Class updateById(@PathVariable("classId") Long classId, Class new_class) {
+        Class _class = classRepository.findOne(classId);
+        return classRepository.save(_class);
+    }
+
+    @DeleteMapping("class/{classId}")
+    public Class deleteById(@PathVariable("classId") Long classId) {
+        Class _class = classRepository.findOne(classId);
+        _class.getStudents().clear();
+
+        classRepository.save(_class);
+        classRepository.delete(classId);
+        return _class;
+    }
 }
--- src/main/java/io/ermdev/alice/repository/StudentRepository.java
+++ src/main/java/io/ermdev/alice/repository/StudentRepository.java
@@ -2,7 +2,6 @@ package io.ermdev.alice.repository;
 
 import io.ermdev.alice.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
