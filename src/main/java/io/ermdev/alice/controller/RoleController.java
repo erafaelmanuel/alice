5d9f26b8b2bc4247b7e1ba6e825ebb2a60efe3f4
erafaelmanuel <erafaelmanuel@gmail.com>
2017-12-23 23:57:47 +0800

    Update the mapper library dependency. And finish the role endpoint.

--- src/main/java/io/ermdev/alice/controller/RoleController.java
+++ src/main/java/io/ermdev/alice/controller/RoleController.java
@@ -1,12 +1,13 @@
 package io.ermdev.alice.controller;
 
 import io.ermdev.alice.dto.RoleDto;
+import io.ermdev.alice.entity.Role;
 import io.ermdev.alice.repository.RoleRepository;
 import io.ermdev.mapfierj.SimpleMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class RoleController {
@@ -21,13 +22,25 @@ public class RoleController {
     }
 
     @GetMapping("role/{roleId}")
-    public RoleDto getById(@PathVariable("roleId") long roleId) {
+    public RoleDto getById(@PathVariable("roleId") Long roleId) {
         return mapper.set(roleRepository.findById(roleId)).mapTo(RoleDto.class);
     }
 
     @GetMapping("role/all")
-    public RoleDto getAll() {
-        return null;
+    public List<RoleDto> getAll() {
+        return mapper.set(roleRepository.findAll()).mapToList(RoleDto.class);
     }
 
-}g
+    @PostMapping("role/add")
+    public RoleDto add(@RequestBody Role role) {
+        return mapper.set(roleRepository.save(role)).mapTo(RoleDto.class);
+    }
+
+    @DeleteMapping("role/{roleId}")
+    public RoleDto deleteById(@PathVariable("roleId") Long roleId) {
+        Role role = roleRepository.findById(roleId);
+        roleRepository.delete(role);
+        return mapper.set(role).mapTo(RoleDto.class);
+    }
+
+}
