f88fa5017d75ac31a289c5f255b2018c8cf725d8
erafaelmanuel <erafaelmanuel@gmail.com>
2017-12-23 22:29:42 +0800

    Start working on role controller.

--- src/main/java/io/ermdev/alice/controller/RoleController.java
+++ src/main/java/io/ermdev/alice/controller/RoleController.java
@@ -1,8 +1,33 @@
 package io.ermdev.alice.controller;
 
+import io.ermdev.alice.dto.RoleDto;
+import io.ermdev.alice.repository.RoleRepository;
+import io.ermdev.mapfierj.SimpleMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RoleController {
 
-}
+    private RoleRepository roleRepository;
+    private SimpleMapper mapper;
+
+    @Autowired
+    public RoleController(RoleRepository roleRepository, SimpleMapper mapper) {
+        this.roleRepository = roleRepository;
+        this.mapper = mapper;
+    }
+
+    @GetMapping("role/{roleId}")
+    public RoleDto getById(@PathVariable("roleId") long roleId) {
+        return mapper.set(roleRepository.findById(roleId)).mapTo(RoleDto.class);
+    }
+
+    @GetMapping("role/all")
+    public RoleDto getAll() {
+        return null;
+    }
+
+}g
